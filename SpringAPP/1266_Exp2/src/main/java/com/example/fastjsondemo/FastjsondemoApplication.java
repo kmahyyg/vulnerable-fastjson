3d04b46607dc7e6e023d93a94de907dde13360f5
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2021-07-29 13:38:41 +0800

    init commit

---
+++ SpringAPP/1266_Exp2/src/main/java/com/example/fastjsondemo/FastjsondemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastjsondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastjsondemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastjsondemoApplication.class, args);
+	}
+
+}
