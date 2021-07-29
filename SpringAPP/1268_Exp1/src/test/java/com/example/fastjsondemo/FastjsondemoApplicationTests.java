d3775710bbac8c6a1ec7e9d90ddae654e919e46c
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2021-07-29 13:38:51 +0800

    init commit

---
+++ SpringAPP/1268_Exp1/src/test/java/com/example/fastjsondemo/FastjsondemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastjsondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastjsondemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
