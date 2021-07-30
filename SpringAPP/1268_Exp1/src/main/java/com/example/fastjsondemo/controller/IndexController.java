1e112bbf466da6e964d5c6f9381055319a0e4b73
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2021-07-30 13:58:42 +0800

    update dependencies
    EvilLdAPServer is requirement.

--- SpringAPP/1268_Exp1/src/main/java/com/example/fastjsondemo/controller/IndexController.java
+++ SpringAPP/1268_Exp1/src/main/java/com/example/fastjsondemo/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.fastjsondemo.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.ParserConfig;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,32 @@ public class IndexController {
     protected String ParseTxt(@RequestBody String body){
         try {
             Object obj = JSON.parse(body);
+            return obj.toString();
+        } catch (Exception e){
+            e.printStackTrace();
+            return e.toString();
+        }
+    }
+
+    @RequestMapping("/parseObjAuto")
+    protected String ParseObjAuto(@RequestBody String body){
+        try {
+            ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+            Object obj = JSON.parseObject(body);
+            ParserConfig.getGlobalInstance().setAutoTypeSupport(false);
+            return obj.toString();
+        } catch (Exception e){
+            e.printStackTrace();
+            return e.toString();
+        }
+    }
+
+    @RequestMapping("/parseTxtAuto")
+    protected String ParseTxtAuto(@RequestBody String body){
+        try {
+            ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+            Object obj = JSON.parse(body);
+            ParserConfig.getGlobalInstance().setAutoTypeSupport(false);
             return obj.toString();
         } catch (Exception e){
             e.printStackTrace();
